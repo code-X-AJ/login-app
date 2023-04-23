105288aacccd74f83a32b7f39c8e846700fbf0e6
thisisaj1999 <atuljoshi94170@gmail.com>
2023-04-23 13:44:50 +0530

    Login Form done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
-import React from 'react'
-
+import React from 'react';
+import './styles.css';
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+  return <div>App</div>;
+};
 
-export default App
+export default App;
